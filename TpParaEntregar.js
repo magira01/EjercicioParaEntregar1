8b7f61a71d875c5d709df18a1bb99489be5a0148
magira01 <magira01@hotmail.com>
2021-11-09 13:53:48 -0300

    first commit

---
+++ TpParaEntregar.js
@@ -0,0 +1,39 @@
+var mes=8
+console.log("Ingrese un mes en forme de numero ejemplo: Enero= 1");
+if(mes== 1)
+console.log("el mes ingresado fue Enero")
+else
+if (mes==2)
+console.log("el mes ingresado fue Febrero")
+else
+if(mes==3)
+console.log("el mes ingresado fue marzo")
+else
+if (mes==4)
+console.log("el mes ingresado fue abril")
+else
+if(mes==5)
+console.log("el mes ingresado es mayo")
+else
+if(mes==6)
+console.log("el mes ingresado es junio")
+else
+if(mes==7)
+console.log("el mes ingresado es julio")
+if(mes==8)
+console.log("el mes ingresado es agosto")
+else
+if(mes==9)
+console.log("el mes ingresado es septiembre")
+else
+if(mes==10)
+console.log("el mes ingresado es octubre")
+else
+if(mes==11)
+console.log("el mes ingresado es noviembre")
+else
+if(mes==12)
+console.log("el mes ingresado es diciembre")
+else
+if(mes>=13)
+console.log("el mes ingresado no existe")
